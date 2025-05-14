77ba1a288ae5196ec615b70bd40d5b81127e559b
e <e@e.cl>
2025-05-14 17:19:48 -0400

    Feat: Initial commit para el frontend hecho en flask

---
+++ app-front/main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import os
+import json
+from app import app
+from flask import request, redirect, url_for, render_template
+
+
+@app.route('/Facegate/')
+def index_form():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(port=8001)
