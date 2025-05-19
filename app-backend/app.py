c518a2d778d7a6d5eed4ce1b58d0f29a0a4ec012
patricioespinozaa <patricio.espinoza.a@ug.uchile.cl>
2025-05-18 20:17:21 -0400

    FIX: Review changes

--- app-backend/app.py
+++ app-backend/app.py
@@ -13,7 +13,10 @@ model: InceptionResnetV1 = InceptionResnetV1(pretrained='vggface2').eval()
 # Labels or additional classes for face recognition
 FILENAME_FACE_CLASSES: str = 'face_class_index.json'
 
+face_class_index: Optional[Dict[str, Any]] = None
+
 try:
-    face_class_index: Optional[Dict[str, Any]] = json.load(open(FILENAME_FACE_CLASSES))
+    with open(FILENAME_FACE_CLASSES) as f:
+        face_class_index = json.load(f)
 except FileNotFoundError:
     face_class_index = None
