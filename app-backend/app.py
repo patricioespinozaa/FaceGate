7918cdcdd56f092664b8dfc663e8872cf978b820
e <e@e.cl>
2025-05-20 12:42:41 -0400

    chore: cors policy

--- app-backend/app.py
+++ app-backend/app.py
@@ -3,10 +3,12 @@ import json
 import os
 from typing import Optional, Dict, Any
 from flask import Flask
+from flask_cors import CORS
 
 #app: Flask = Flask(__name__)
 db_path = os.path.join(os.path.dirname(__file__), 'DB_UCampus')
 app: Flask = Flask(__name__, static_url_path='/static', static_folder=db_path)
+CORS(app, resources={r"/facegate/*": {"origins": "http://localhost:8001"}})
 
 # Load the InceptionResnetV1 model pretrained with VGGFace2
 from facenet_pytorch import InceptionResnetV1
