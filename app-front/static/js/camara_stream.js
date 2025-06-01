a67105e42bed722105158e3c96327866a2724ccd
Chileandude <ignacioS.Ghisolfo@gmail.com>
2025-06-01 11:40:03 -0400

    FEAT: se agrega que cambie el stream por la foto tomada dentro de la caja cámara

--- app-front/static/js/camara_stream.js
+++ app-front/static/js/camara_stream.js
@@ -25,6 +25,16 @@ document.getElementById('captura').addEventListener('click', function () {
         if (blob) {
             window.lastCaptureBlob = blob;  // Guarda global para que otro JS lo use
             console.log("Foto capturada y almacenada en window.lastCaptureBlob");
+            //Cambiar stream por la foto 
+            const cameraBody = document.getElementById('camera-body-camara');
+            cameraBody.innerHTML = ''; 
+            const img = document.createElement('img');
+            img.src = URL.createObjectURL(window.lastCaptureBlob);
+            img.id = 'captured-image';
+            img.style.maxWidth = '100%';
+            img.style.maxHeight = '100%';
+            img.style.objectFit = 'contain';
+            cameraBody.appendChild(img);
         } else {
             console.error("No se pudo crear el blob de la imagen");
         }
