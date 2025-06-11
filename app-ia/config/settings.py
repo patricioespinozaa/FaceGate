4f228d1ef4c932ffd571c3216bf750bca4740297
lauraflm <laura.maldonado@ug.uchile.cl>
2025-06-10 22:00:35 -0400

    FIX: Gitignore para eliminar archivos .pyc

--- app-ia/config/settings.py
+++ app-ia/config/settings.py
@@ -9,3 +9,8 @@ DB_CREDENTIALS = {
     "password": "piagrupo3",
     "database": "ucampus_db"
 }
+
+APP_IA_CORS = ""
+APP_HTTPS = ""
+API_URL = "https://grupo3.juan.cl/facegate/app-ia/predict"  # API endpoint for predictions
+DISTANCIA_COSENO = 0.5
