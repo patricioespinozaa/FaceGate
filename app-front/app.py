82fc33e424da277c996363ddd75e1d869bb414f8
Chileandude <ignacioS.Ghisolfo@gmail.com>
2025-07-10 00:50:38 -0400

    feat: stream desde la camara guardia a la vista student

--- app-front/app.py
+++ app-front/app.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*- 
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__,
             static_url_path='/facegate/app-front/static',
             static_folder='static',
             template_folder='templates')
+
+socketio = SocketIO(app, cors_allowed_origins="*")
